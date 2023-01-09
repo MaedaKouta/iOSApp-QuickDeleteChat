bdcb4e0c40d2a928f44dcc6567bfd3a036e08074
Kota <maedakoutaa@gmail.com>
2023-01-09 11:53:41 +0900

    add: firebaseのコード追加

---
+++ QuickDeleteChat/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  QuickDeleteChat
+//
+//  Created by 前田航汰 on 2023/01/09.
+//
+
+import Foundation
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
