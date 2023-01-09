9370f80420b33343826350e59ee4ceb2b6053007
Kota <maedakoutaa@gmail.com>
2023-01-09 09:59:07 +0900

    Initial Commit

---
+++ QuickDeleteChat/QuickDeleteChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickDeleteChatApp.swift
+//  QuickDeleteChat
+//
+//  Created by 前田航汰 on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct QuickDeleteChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
