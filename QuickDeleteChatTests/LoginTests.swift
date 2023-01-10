a13fa513b86e91b242db5b4bb60460c747b29b68
Kota <maedakoutaa@gmail.com>
2023-01-10 09:43:09 +0900

    add: テストファイルの作成

--- QuickDeleteChatTests/LoginTests.swift
+++ QuickDeleteChatTests/LoginTests.swift
@@ -10,27 +10,42 @@ import XCTest
 
 final class LoginTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let authManager = AuthManager()
+
+    func test_ログインが成功すること() {
+        let mailAdress = "test@testmail.com"
+        let password = "000000"
+
+        authManager.Login(email: mailAdress, password: password, complition: { isSuccess in
+            XCTAssertEqual(isSuccess, true)
+        })
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_ログインのエラーが返ってくること_パスワードミス() {
+        let mailAdress = "test@testmail.com"
+        let password = "111111"
+
+        authManager.Login(email: mailAdress, password: password, complition: { isSuccess in
+            XCTAssertEqual(isSuccess, false)
+        })
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_ログインのエラーが返ってくること_メールアドレスミス() {
+        let mailAdress = "test.miss@testmail.com"
+        let password = "000000"
+
+        authManager.Login(email: mailAdress, password: password, complition: { isSuccess in
+            XCTAssertEqual(isSuccess, false)
+        })
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_ログインのエラーが返ってくること_パスワード＆メールアドレスミス() {
+        let mailAdress = "test.miss@testmail.com"
+        let password = "111111"
+
+        authManager.Login(email: mailAdress, password: password, complition: { isSuccess in
+            XCTAssertEqual(isSuccess, false)
+        })
     }
 
 }
